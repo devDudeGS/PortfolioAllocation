667f7de66ce86958dda5ba8c8131623472f01fb5
Gregory Stevens <networkwithgreg@gmail.com>
2021-11-04 21:46:55 -0400

    Added pseudocode for 401k rebalance

---
+++ Rebalance401k.py
@@ -0,0 +1,13 @@
+# 1. Input: total of all $$ in all retirement accounts
+# 2. Input: current $$ totals in each 401k account
+# 3. Add totals together, and calculate proportion of each to sum total
+# 4. Find % of sum total to retirement total
+# 5. Compare sum total % to goal proportion total % (for 401k categories)
+# 6. Adjust goal proportion to match actual proportion ideal
+# 7. Use actual proportion ideal to calculate ideal $$ totals for each (use for later comparison)
+# 8. Find absolute % for each category / account
+# i.e. acct allocation % * cat allocation % = absolute %
+# or acct allocation / all acct allocations (100% - non-participating accts) * cat allocation = absolute %
+# 9. Output: relative % for each category / account
+# i.e. absolute cat/acct % / absolute acct total % = relative %
+# Use this to update existing percentages in accts, but for future stick to ideal in spreadsheet
