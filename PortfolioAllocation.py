b15c9ab552a10431fe3e78b5b69482df9c4271e2
Gregory Stevens <networkwithgreg@gmail.com>
2023-06-16 22:31:16 -0400

    Heavily cleaned up portfolio alloc script

--- PortfolioAllocation.py
+++ PortfolioAllocation.py
@@ -2,34 +2,88 @@ import csv
 import numpy as np
 
 
-# SCRIPT IS WIP
-def get_allocations(starting_portfolio, ideal_allocation, cash_ira_g, cash_ira_j, prices_ira_g, prices_ira_j):
-    # ideal cash
+def balance_iras():
+    asset_classes_total = 7
+
+    all_data_np = get_data()
+    starting_portfolio, goal_proportions, cash_ira_self, cash_ira_spouse, prices_ira_self, prices_ira_spouse = get_params(
+        all_data_np, asset_classes_total)
+
     ideal_cash_allocation = get_ideal_cash_allocation(
-        starting_portfolio, ideal_allocation)
+        starting_portfolio, goal_proportions)
+    cash_allocation_ira_self, cash_allocation_ira_spouse = get_cash_per_asset_class(
+        starting_portfolio, goal_proportions, cash_ira_self, prices_ira_self, cash_ira_spouse, prices_ira_spouse, asset_classes_total)
+    shares_allocation_ira_self, shares_allocation_ira_spouse = get_shares_to_buy(
+        starting_portfolio, goal_proportions, cash_allocation_ira_self, prices_ira_self, cash_allocation_ira_spouse, prices_ira_spouse, asset_classes_total)
 
-    # how much cash to allocate to each security
-    cash_allocation_ira_g = get_cash_allocation(
-        starting_portfolio, ideal_allocation, cash_ira_g, prices_ira_g)
-    cash_allocation_ira_j = get_cash_allocation(
-        starting_portfolio, ideal_allocation, cash_ira_j, prices_ira_j)
-
-    # how many shares to buy with cash
-    shares_allocation_ira_g = get_shares_allocation(
-        starting_portfolio, ideal_allocation, cash_allocation_ira_g, prices_ira_g)
-    shares_allocation_ira_j = get_shares_allocation(
-        starting_portfolio, ideal_allocation, cash_allocation_ira_j, prices_ira_j)
-
-    return shares_allocation_ira_g, shares_allocation_ira_j, ideal_cash_allocation
+    print_results(shares_allocation_ira_self, shares_allocation_ira_spouse, prices_ira_self, prices_ira_spouse,
+                  cash_ira_self, cash_ira_spouse, starting_portfolio, goal_proportions, ideal_cash_allocation)
 
 
-def get_cash_allocation(portfolio, allocation, cash, prices):
+def get_data():
+    with open("data/RetirementData.csv", "r") as file:
+        reader = csv.reader(file)
+        data = list(reader)
+
+    return np.array(data)
+
+
+def get_params(all_data_np, asset_classes_total):
+    final_row_index = asset_classes_total + 1
+    goal_proportions = all_data_np[1:final_row_index, 1].astype(np.float)
+
+    starting_portfolio = np.zeros(asset_classes_total)
+    for i in range(0, asset_classes_total):
+        asset_class = all_data_np[i + 1, 2:6].astype(np.float)
+        starting_portfolio[i] = np.round(np.sum(asset_class), 2)
+
+    cash_ira_self = float(all_data_np[final_row_index][2])
+    cash_ira_spouse = float(all_data_np[final_row_index][3])
+
+    prices_ira_self = all_data_np[1:final_row_index, 6].astype(np.float)
+    prices_ira_spouse = all_data_np[1:final_row_index, 7].astype(np.float)
+
+    return starting_portfolio, goal_proportions, cash_ira_self, cash_ira_spouse, prices_ira_self, prices_ira_spouse
+
+
+def get_ideal_cash_allocation(portfolio, allocation):
+    """
+    Finds the ideal cash amount to make the portfolio match the allocation goals
+    """
+
+    ideal_allocation_portfolio = allocation * np.sum(portfolio)
+    portfolio_diff = portfolio - ideal_allocation_portfolio
+    max_portfolio_value = portfolio[np.argmax(portfolio_diff)]
+    max_allocation_value = allocation[np.argmax(portfolio_diff)]
+    ideal_portfolio_total = max_portfolio_value / \
+        (max_allocation_value * 100) * 100
+
+    ideal_cash_allocation = np.round(
+        ideal_portfolio_total - np.sum(portfolio), 2)
+
+    return ideal_cash_allocation
+
+
+def get_cash_per_asset_class(starting_portfolio, goal_proportions, cash_ira_self, prices_ira_self, cash_ira_spouse, prices_ira_spouse, asset_classes_total):
+    """
+    Finds the amount of cash to allocate to each asset class
+    """
+
+    cash_allocation_ira_self = get_cash_allocation(
+        starting_portfolio, goal_proportions, cash_ira_self, prices_ira_self, asset_classes_total)
+    cash_allocation_ira_spouse = get_cash_allocation(
+        starting_portfolio, goal_proportions, cash_ira_spouse, prices_ira_spouse, asset_classes_total)
+
+    return cash_allocation_ira_self, cash_allocation_ira_spouse
+
+
+def get_cash_allocation(portfolio, allocation, cash, prices, asset_classes_total):
     # amount of cash to reach ideal
     ideal_totals = allocation * (np.sum(portfolio) + cash)
     diff_totals = portfolio - ideal_totals
 
     # create array of securities to add to
-    revised_totals = np.zeros(7)
+    revised_totals = np.zeros(asset_classes_total)
     for i in range(len(diff_totals)):
         if diff_totals[i] < 0 and prices[i] != -1.00:
             revised_totals[i] = diff_totals[i] * -1
@@ -42,8 +96,21 @@ def get_cash_allocation(portfolio, allocation, cash, prices):
     return np.round(proportion_totals * cash, 2)
 
 
-def get_shares_allocation(starting_portfolio, ideal_allocation, cash_allocation, prices):
-    shares_total = np.zeros(7)
+def get_shares_to_buy(starting_portfolio, goal_proportions, cash_allocation_ira_self, prices_ira_self, cash_allocation_ira_spouse, prices_ira_spouse, asset_classes_total):
+    """
+    Finds the number of shares to buy for each asset class
+    """
+
+    shares_allocation_ira_self = get_shares_allocation(
+        starting_portfolio, goal_proportions, cash_allocation_ira_self, prices_ira_self, asset_classes_total)
+    shares_allocation_ira_spouse = get_shares_allocation(
+        starting_portfolio, goal_proportions, cash_allocation_ira_spouse, prices_ira_spouse, asset_classes_total)
+
+    return shares_allocation_ira_self, shares_allocation_ira_spouse
+
+
+def get_shares_allocation(starting_portfolio, ideal_allocation, cash_allocation, prices, asset_classes_total):
+    shares_total = np.zeros(asset_classes_total)
 
     while True:
         shares, remainder = np.divmod(cash_allocation, prices)
@@ -51,10 +118,35 @@ def get_shares_allocation(starting_portfolio, ideal_allocation, cash_allocation,
             break
         else:
             cash_allocation = get_cash_allocation(
-                starting_portfolio, ideal_allocation, np.sum(remainder), prices)
+                starting_portfolio, ideal_allocation, np.sum(remainder), prices, asset_classes_total)
             shares_total += shares
 
     return shares_total
+
+
+def print_results(shares_ira_self, shares_ira_spouse, prices_ira_self, prices_ira_spouse, cash_ira_self, cash_ira_spouse, starting_portfolio, goal_proportions, ideal_cash_allocation):
+    increase_ira_self = shares_ira_self * prices_ira_self
+    increase_ira_spouse = shares_ira_spouse * prices_ira_spouse
+
+    # print results
+    print()
+    print("Goal proportions:       ", goal_proportions)
+    print("Starting proportions:   ", get_proportions(starting_portfolio))
+    print("Ending proportions:     ", get_proportions(
+        starting_portfolio + increase_ira_self + increase_ira_spouse))
+    print()
+    print("G's IRA cash start:      ", round(cash_ira_self, 2))
+    print("Shares of G's IRA:       ", shares_ira_self)
+    print("G's IRA cash remaining:  ", round(
+        cash_ira_self - np.sum(increase_ira_self), 2))
+    print()
+    print("J's IRA cash start:    ", round(cash_ira_spouse, 2))
+    print("Shares of J's IRA:     ", shares_ira_spouse)
+    print("J's IRA cash remaining:", round(
+        cash_ira_spouse - np.sum(increase_ira_spouse), 2))
+    print()
+    print("Ideal cash addition:    ", ideal_cash_allocation)
+    print()
 
 
 def get_proportions(arr):
@@ -64,91 +156,6 @@ def get_proportions(arr):
         proportions[i] = arr[i] / arr_total
 
     return np.round(proportions, 3)
-
-
-def get_ideal_cash_allocation(portfolio, allocation):
-    ideal_allocation_portfolio = allocation * np.sum(portfolio)
-    portfolio_diff = portfolio - ideal_allocation_portfolio
-    max_portfolio_value = portfolio[np.argmax(portfolio_diff)]
-    max_allocation_value = allocation[np.argmax(portfolio_diff)]
-    ideal_portfolio_total = max_portfolio_value / \
-        (max_allocation_value * 100) * 100
-
-    return np.round(ideal_portfolio_total - np.sum(portfolio), 2)
-
-
-def input_numbers(all_data_np):
-    # percentages aiming for
-    goal_proportions = all_data_np[1:8, 1]
-    goal_proportions = goal_proportions.astype(np.float)
-
-    # current data
-    security_type_1 = all_data_np[1, 2:6]
-    security_type_1 = security_type_1.astype(np.float)
-    security_type_1_sum = np.round(np.sum(security_type_1), 2)
-    security_type_2 = all_data_np[2, 2:6]
-    security_type_2 = security_type_2.astype(np.float)
-    security_type_2_sum = np.round(np.sum(security_type_2), 2)
-    security_type_3 = all_data_np[3, 2:6]
-    security_type_3 = security_type_3.astype(np.float)
-    security_type_3_sum = np.round(np.sum(security_type_3), 2)
-    security_type_4 = all_data_np[4, 2:6]
-    security_type_4 = security_type_4.astype(np.float)
-    security_type_4_sum = np.round(np.sum(security_type_4), 2)
-    security_type_5 = all_data_np[5, 2:6]
-    security_type_5 = security_type_5.astype(np.float)
-    security_type_5_sum = np.round(np.sum(security_type_5), 2)
-    security_type_6 = all_data_np[6, 2:6]
-    security_type_6 = security_type_6.astype(np.float)
-    security_type_6_sum = np.round(np.sum(security_type_6), 2)
-    security_type_7 = all_data_np[7, 2:6]
-    security_type_7 = security_type_7.astype(np.float)
-    security_type_7_sum = np.round(np.sum(security_type_7), 2)
-    starting_portfolio = np.array([security_type_1_sum, security_type_2_sum, security_type_3_sum, security_type_4_sum,
-                                   security_type_5_sum, security_type_6_sum, security_type_7_sum])
-
-    cash_ira_g = float(all_data_np[8][2])
-    cash_ira_j = float(all_data_np[8][3])
-
-    ira_prices_g = all_data_np[1:8, 6]
-    ira_prices_g = ira_prices_g.astype(np.float)
-    ira_prices_j = all_data_np[1:8, 7]
-    ira_prices_j = ira_prices_j.astype(np.float)
-
-    # output shares
-    shares_ira_g, shares_ira_j, ideal_cash_allocation = get_allocations(starting_portfolio, goal_proportions,
-                                                                        cash_ira_g, cash_ira_j, ira_prices_g,
-                                                                        ira_prices_j)
-    increase_ira_g = shares_ira_g * ira_prices_g
-    increase_ira_j = shares_ira_j * ira_prices_j
-
-    # print results
-    print("Goal proportions:       ", goal_proportions)
-    print("Starting proportions:   ", get_proportions(starting_portfolio))
-    print("Ending proportions:     ", get_proportions(
-        starting_portfolio + increase_ira_g + increase_ira_j))
-    print("G's IRA cash start:      ", round(cash_ira_g, 2))
-    print("Shares of G's IRA:       ", shares_ira_g)
-    print("G's IRA cash remaining:  ", round(
-        cash_ira_g - np.sum(increase_ira_g), 2))
-    print("J's IRA cash start:    ", round(cash_ira_j, 2))
-    print("Shares of J's IRA:     ", shares_ira_j)
-    print("J's IRA cash remaining:", round(
-        cash_ira_j - np.sum(increase_ira_j), 2))
-    print("Ideal cash addition:    ", ideal_cash_allocation)
-
-
-def get_data():
-    with open("data/RetirementData.csv", "r") as file:
-        reader = csv.reader(file)
-        data = list(reader)
-
-    return np.array(data)
-
-
-def balance_iras():
-    all_data_np = get_data()
-    input_numbers(all_data_np)
 
 
 if __name__ == "__main__":
